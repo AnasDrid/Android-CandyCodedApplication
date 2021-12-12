c5b6745247dead045ac1562c3b49dc7e4ec09126
Anas Drid <ga_drid@esi.dz>
2021-12-12 05:06:35 +0100

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,10 +1,13 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
 import android.widget.ImageView;
 import com.squareup.picasso.Picasso;
 import java.io.IOException;
@@ -30,6 +33,14 @@ public class InfoActivity extends AppCompatActivity {
 
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
+    public void createMapIntent(View view) {
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapInent = new Intent(Intent.ACTION_VIEW,uri);
+        mapInent.setPackage("com.google.android.apps.maps");
+        if (mapInent.resolveActivity(getPackageManager())!=null){
+            startActivity(mapInent);
+        }
+    }
     // ***
 
     // ***
